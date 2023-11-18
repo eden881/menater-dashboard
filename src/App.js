012bebe6341e43ef535fc0f679af2740dcdaaaad
Eden Yemini <eden881@gmail.com>
2023-11-18 18:42:31 +0200

    Inform react router of GH pages basename

--- src/App.js
+++ src/App.js
@@ -6,23 +6,28 @@ import Base from "./routes/Base";
 import Dashboard from "./routes/Dashboard";
 import NatarList from "./routes/NatarList";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Base />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <Dashboard />,
+        },
+        {
+          path: "natars",
+          element: <NatarList />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Base />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <Dashboard />,
-      },
-      {
-        path: "natars",
-        element: <NatarList />,
-      },
-    ],
-  },
-]);
+    basename: "/menater-dashboard",
+  }
+);
 
 function App() {
   return (
